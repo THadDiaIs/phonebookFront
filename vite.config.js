64ca17871338dcf72c7fe39e675fc66e01d98b6c
Anton Hunter <sdia7sdia@gmail.com>
2024-03-29 11:26:17 +1030

    configured proxy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api":{
-        target: "http://localhost:3490,
+        target: "http://localhost:3490",
         changeOrigin: true,
 },
 },
